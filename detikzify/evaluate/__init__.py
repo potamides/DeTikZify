0284f9e2d7ffbf6a7d1d721bf7127a221d01eaff
Jonas Belouadi <jonas.belouadi@posteo.net>
2024-02-20 13:55:35 +0100

    build(evaluate): make patchsim metric non-optional
    This metric is used by the mcts inference code, so the metric is now a
    required dependency. The other metrics remain optional.

--- detikzify/evaluate/__init__.py
+++ detikzify/evaluate/__init__.py
@@ -1,10 +1,25 @@
-try:
-    from .patchsim import *
-    from .eed import *
-    from .kid import *
-    from .crystalbleu import *
-except ImportError:
-    raise ValueError(
-        "Missing dependencies: "
-        "Install this project with the [evaluate] feature name!"
-    )
+from importlib import import_module
+from typing import Any
+
+from .patchsim import * # this metric is used by MCTS, so it is not optional
+
+__all__ = ["PatchSim", "CrystalBLEU", "KernelInceptionDistance", "TexEditDistance"] # type: ignore
+
+# lazy import optional metrics (https://peps.python.org/pep-0562/)
+def __getattr__(name) -> Any:
+    def load(metric):
+        return getattr(import_module("." + metric, __name__), name)
+    try:
+        match name:
+            case "CrystalBLEU":
+                return load("crystalbleu")
+            case "KernelInceptionDistance":
+                return load("kid")
+            case "TexEditDistance":
+                return load("eed")
+    except ImportError:
+        raise ValueError(
+            "Missing dependencies: "
+            "Install this project with the [evaluate] feature name!"
+        )
+    return import_module("." + name, __name__)
