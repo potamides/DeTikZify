5022cd9a750649439a12850ee90e6a971ee56e78
Jonas Belouadi <53401822+potamides@users.noreply.github.com>
2024-07-21 17:01:15 +0200

    fix(evaluate): set dtype correctly

--- detikzify/evaluate/kid.py
+++ detikzify/evaluate/kid.py
@@ -16,7 +16,7 @@ class TimmFeatureWrapper(nn.Module):
         super().__init__()
         self.model_name = model_name
         self.device = device
-        self.set_dtype(dtype)
+        self.dtype = dtype
 
     @cached_property
     def model(self):
