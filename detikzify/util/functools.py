7c3f50290e41faed816e20b0375eac55ca41070b
Jonas Belouadi <jonas.belouadi@posteo.net>
2024-09-12 10:17:12 +0900

    feat(util): add helper to listify generators

--- detikzify/util/functools.py
+++ detikzify/util/functools.py
@@ -38,3 +38,36 @@ def cast_cache(cast_func: Callable[..., Any]):
         return wraps(func)(MethodDecoratorAdapter(func))
 
     return decorator
+
+# https://stackoverflow.com/a/12377059
+def listify(fn=None, wrapper=list):
+    """
+    A decorator which wraps a function's return value in ``list(...)``.
+
+    Useful when an algorithm can be expressed more cleanly as a generator but
+    the function should return an list.
+
+    Example::
+
+        >>> @listify
+        ... def get_lengths(iterable):
+        ...     for i in iterable:
+        ...         yield len(i)
+        >>> get_lengths(["spam", "eggs"])
+        [4, 4]
+        >>>
+        >>> @listify(wrapper=tuple)
+        ... def get_lengths_tuple(iterable):
+        ...     for i in iterable:
+        ...         yield len(i)
+        >>> get_lengths_tuple(["foo", "bar"])
+        (3, 3)
+    """
+    def listify_return(fn):
+        @wraps(fn)
+        def listify_helper(*args, **kw):
+            return wrapper(fn(*args, **kw))
+        return listify_helper
+    if fn is None:
+        return listify_return
+    return listify_return(fn)
