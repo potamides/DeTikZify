0c45c36ab4f73829ab2a7a5ee98481c6ce7c553e
Jonas Belouadi <jonas.belouadi@posteo.net>
2024-09-12 10:14:28 +0900

    feat(model): add a helper for loading model and processor

--- detikzify/model/__init__.py
+++ detikzify/model/__init__.py
@@ -1,4 +1,34 @@
+from safetensors.torch import load_file
+from transformers import (
+    AutoConfig,
+    AutoImageProcessor,
+    AutoModelForVision2Seq,
+    AutoProcessor,
+)
+
 from .configuration_detikzify import *
 from .image_processing_detikzify import *
 from .modeling_detikzify import *
 from .processing_detikzify import *
+
+def register():
+    try:
+        AutoConfig.register("detikzify", DetikzifyConfig)
+        AutoModelForVision2Seq.register(DetikzifyConfig, DetikzifyForConditionalGeneration)
+        AutoImageProcessor.register(DetikzifyConfig, DetikzifyImageProcessor)
+        AutoProcessor.register(DetikzifyConfig, DetikzifyProcessor)
+    except ValueError:
+        pass # already registered
+
+def load(model_name_or_path, modality_projector=None, **kwargs):
+    register()
+    processor = AutoProcessor.from_pretrained(model_name_or_path)
+    model = AutoModelForVision2Seq.from_pretrained(model_name_or_path, **kwargs)
+
+    if modality_projector is not None:
+        model.load_state_dict(load_file(
+            filename=modality_projector,
+            device=str(model.device)
+        ))
+
+    return model, processor
