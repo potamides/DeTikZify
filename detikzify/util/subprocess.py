3356992617091cfa744fa15757735c03eb8fa9d7
Jonas Belouadi <53401822+potamides@users.noreply.github.com>
2025-06-03 14:03:12 +0200

    fix(util): workaround for missing os.killpg on windows

--- detikzify/util/subprocess.py
+++ detikzify/util/subprocess.py
@@ -1,32 +1,48 @@
-from os import killpg, getpgid
-from subprocess import Popen, TimeoutExpired, CalledProcessError, CompletedProcess, PIPE
-from signal import SIGKILL
+import os
+import signal
+import subprocess
 
-def safe_killpg(pid, signal):
-    try:
-        killpg(getpgid(pid), signal)
-    except ProcessLookupError:
-        pass # Supress the race condition error; bpo-40550.
-
-# Patch subprocess.run and subprocess.check_output to also kill children of the
-# started process on timeouts (cf.
+# Patched subprocess.run and subprocess.check_output that also kill children of
+# the started process on timeouts (cf.
 # https://alexandra-zaharia.github.io/posts/kill-subprocess-and-its-children-on-timeout-python/)
+class _Popen(subprocess.Popen):
+    def __init__(self, *args, **kwargs):
+        if os.name == "nt":
+            return super().__init__(
+                *args, creationflags=subprocess.CREATE_NEW_PROCESS_GROUP, **kwargs
+            )
+        return super().__init__(*args, start_new_session=True, **kwargs)
+
+    def safe_killpg(self):
+        try:
+            if os.name == "nt":
+                # https://stackoverflow.com/a/28609523
+                return os.kill(self.pid, signal.CTRL_BREAK_EVENT)
+            return os.killpg(os.getpgid(self.pid), signal.SIGKILL)
+        except ProcessLookupError:
+            pass # Supress the race condition error; bpo-40550.
+
 def run(*popenargs, input=None, timeout=None, check=False, **kwargs):
-    with Popen(*popenargs, start_new_session=True, **kwargs) as process:
+    with _Popen(*popenargs, **kwargs) as process:
         try:
             stdout, stderr = process.communicate(input, timeout=timeout)
-        except TimeoutExpired:
-            safe_killpg(process.pid, SIGKILL)
+        except subprocess.TimeoutExpired:
+            process.safe_killpg()
             process.wait()
             raise
         except:
-            safe_killpg(process.pid, SIGKILL)
+            process.safe_killpg()
             raise
         retcode = process.poll()
         if check and retcode:
-            raise CalledProcessError(retcode, process.args,
-                                     output=stdout, stderr=stderr)
-    return CompletedProcess(process.args, retcode, stdout, stderr) # type: ignore
+            raise subprocess.CalledProcessError(
+                retcode, process.args, output=stdout, stderr=stderr
+            )
+    return subprocess.CompletedProcess(
+        process.args, retcode, stdout, stderr # type: ignore
+    )
 
 def check_output(*popenargs, timeout=None, **kwargs):
-    return run(*popenargs, stdout=PIPE, timeout=timeout, check=True, **kwargs).stdout
+    return run(
+        *popenargs, stdout=subprocess.PIPE, timeout=timeout, check=True, **kwargs
+    ).stdout
